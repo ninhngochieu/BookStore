8dbfb396db37fe3e66c459624513136bebecb285
Ninh Ngoc Hieu <30309069+ninhngochieu@users.noreply.github.com>
2021-02-09 15:19:22 +0700

    Add category context

---
+++ BookStore/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace BookStore.Models
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public partial class Category
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
