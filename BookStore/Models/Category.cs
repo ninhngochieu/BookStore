b95a9063497e8c93a9821d5bbf5b1c37baf887ca
Ninh Ngoc Hieu <30309069+ninhngochieu@users.noreply.github.com>
2021-02-09 15:50:02 +0700

    Add Column active

--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -14,5 +14,6 @@ namespace BookStore.Models
         [Key]
         public long Id { get; set; }
         public string Name { get; set; }
+        public long Active { get; set; }
     }
 }
--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +29,7 @@ namespace BookStore
         {
 
             services.AddControllers();
+            services.AddEntityFrameworkSqlite().AddDbContext<bookstoreContext>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStore", Version = "v1" });
