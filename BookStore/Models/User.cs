1d5b64bef5ae537a275281134608861a66d77244
Ninh Ngoc Hieu <30309069+ninhngochieu@users.noreply.github.com>
2021-02-20 10:44:27 +0700

    Add User and Login view models

---
+++ BookStore/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace BookStore.Models
+{
+    public partial class User
+    {
+        [Key]
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public long? Roles { get; set; }
+    }
+}
---
+++ BookStore/View Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BookStore.ViewModels
+{
+    public class Login
+    {
+
+        public Login()
+        {
+        }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
