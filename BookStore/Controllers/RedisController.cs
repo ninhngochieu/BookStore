f77d2672452bfb2c7fc9e84fe288a5089d689973
Phạm Đăng Trung <55026963+dnhoangson@users.noreply.github.com>
2021-05-13 16:11:41 +0700

    Setting up Redis

---
+++ BookStore/Controllers/RedisController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using BookStore.Models;
+using BookStore.Modules;
+using BookStore.ViewModels.Cart;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        private string[] listStr;
+        private readonly IDatabase redis;
+        private readonly IConnectionMultiplexer _conn;
+        private readonly IMapper _mapper;
+        public RedisController(IConnectionMultiplexer conn, IMapper mapper)
+        {
+            _mapper = mapper;
+            _conn = conn;
+            redis = _conn.GetDatabase();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCartRedis(int userId)
+        {
+            string recordKey = userId.ToString();
+            var cart = await redis.GetRecordAsync<IList<Cart>>(recordKey);
+            
+            if (cart is null)
+            {
+                return NotFound(new { data = "Empty cart", success = true });
+            }
+            //string recordKey = "string_" + DateTime.Now.ToString("yyyyMMdd_hhmm");
+            //listStr = new string[2];
+            //listStr[0] = "Not";
+            //listStr[1] = "Working";
+            //var serializedStr = JsonSerializer.Serialize(listStr);
+            //await redis.StringSetAsync(recordKey, serializedStr);
+
+            await redis.SetRecordAsync(recordKey, listStr);
+
+            //var redisResult = await redis.GetRecordAsync<string[]>(recordKey);
+            //var deserializedStr = JsonSerializer.Deserialize<string[]>(redisResult);
+            //return Ok(cart);
+            return Ok(new { data = _mapper.Map<List<CartViewModel>>(cart), success = true });
+        }
+
+
+        [HttpPost]
+        public void SetCartRedis([FromBody] string value)
+        {
+        }
+
+    }
+}
---
+++ BookStore/Modules/RedisExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using StackExchange.Redis;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BookStore.Modules
+{
+    public static class RedisExtensions
+    {
+        public static IDatabase GetRedis(this IConnectionMultiplexer connectionMultiplexer)
+        {
+            return connectionMultiplexer.GetDatabase();
+        }
+
+        public static async Task SetRecordAsync<T>(this IDatabase redisDatabase,
+                                                string recordId,
+                                                T data,
+                                                TimeSpan? expireTime = null)
+        {
+            //expireTime = expireTime ?? TimeSpan.FromHours(3);
+            expireTime = expireTime ?? TimeSpan.FromSeconds(30);
+
+            var serializedData = JsonSerializer.Serialize(data);
+
+            await redisDatabase.StringSetAsync(recordId, serializedData, expireTime);
+        }
+
+        public static async Task<T> GetRecordAsync<T>(this IDatabase redisDatabase, string recordId)
+        {
+            var serializedData = await redisDatabase.StringGetAsync(recordId);
+
+            if (serializedData.IsNullOrEmpty)
+            {
+                return default;
+            }
+
+            T deserializedData = JsonSerializer.Deserialize<T>(serializedData);
+            return deserializedData;
+        }
+    }
+}
